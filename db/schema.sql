7bc3970780d320e9525f0c4c210e856151390b3d
Bram Cohn <BramCohn@gmail.com>
2025-06-10 20:41:04 -0600

    finished schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,30 @@
--- TODO
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS orders_products;
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    note TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL NOT NULL
+)
+
+CREATE TABLE orders(
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE NOT NULL,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE NOT NULL,
+    quantity INTEGER NOT NULL
+)
